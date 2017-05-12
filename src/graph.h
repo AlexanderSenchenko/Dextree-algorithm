a1a4ac987d91f78359fc25078cb0b80d0f0eb215
Александр <zayzsacha@mail.ru>
2017-05-12 10:52:49 +0700

    new

--- src/graph.h
+++ src/graph.h
@@ -1,6 +1,11 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+struct heapnode {
+	int key;
+	i
+}
+
 int g_i(int i, int j);
 void all(int g[], int len[], int vis[]);
 
--- src/main.c
+++ src/main.c
@@ -4,21 +4,6 @@
 int main()
 {
 	FILE *in = fopen("graph.txt", "r");
-	int graph[36];
-	int len[6];
-	int vis[6];
-
-	for (int i = 0; i < 36; i++) {
-		fscanf(in, "%d", &graph[i]);
-	}
-
-	for (int i = 0; i < 6; i++) {
-		len[i] = 100;
-		vis[i] = -1;
-	}
-	len[0] = 0;
-	
-	all(graph, len, vis);
 	
 	fclose(in);
 	return 0;
